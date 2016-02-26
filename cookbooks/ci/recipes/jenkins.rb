47c6e73c69127c692e65b0555f1819653494b1d7
Filippo Liverani <fili_live@yahoo.it>
2016-02-26 11:56:22 +0100

    Added jenkins service configuration

--- cookbooks/ci/recipes/jenkins.rb
+++ cookbooks/ci/recipes/jenkins.rb
@@ -4,6 +4,10 @@ package 'openjdk-7-jre-headless'
 
 include_recipe 'jenkins::master'
 
+cookbook_file '/etc/default/jenkins' do
+  source 'jenkins'
+end
+
 jenkins_plugin 'delivery-pipeline-plugin'
 jenkins_plugin 'build-pipeline-plugin' do
   notifies :restart, 'service[jenkins]'
